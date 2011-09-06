254c1cb1a9e0b20d07b0ec8ce66a26b23307cdda
Daniel Wallerius <daniel.wallerius@gmail.com>
2011-09-06 14:48:56 +0200

    multiselect to html

---
+++ application/src/main/java/com/fairviewiq/utils/MultiSelectFunctionMember.java
@@ -0,0 +1,11 @@
+package com.fairviewiq.utils;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: danielwallerius
+ * Date: 2011-09-05
+ * Time: 14.19
+ * To change this template use File | Settings | File Templates.
+ */
+public class MultiSelectFunctionMember {
+}
---
+++ application/src/main/webapp/multiSelectGenerator.js
@@ -0,0 +1,7 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: danielwallerius
+ * Date: 2011-09-06
+ * Time: 14.32
+ * To change this template use File | Settings | File Templates.
+ */
