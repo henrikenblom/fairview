63431a9136c8e84cb40a9abe653bfcdce6dfcc2c
Henrik Enblom <henrik.enblom@alfaprint.se>
2011-10-11 14:23:41 +0200

    FVW-78 First attempt at implementing a file upload controller.

---
+++ application/src/main/java/com/fairviewiq/spring/controllers/ProfileImageUploadController.java
@@ -0,0 +1,31 @@
+package com.fairviewiq.spring.controllers;
+
+import com.fairviewiq.beans.FileUploadBean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.SimpleFormController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: henrik
+ * Date: 2011-10-11
+ * Time: 13:51
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Controller
+public class ProfileImageUploadController extends SimpleFormController {
+
+    @Override
+    protected ModelAndView onSubmit(Object command) throws Exception {
+
+        FileUploadBean fileUploadBean = (FileUploadBean) command;
+
+        byte[] file = fileUploadBean.getFile();
+
+        System.err.println("############# FILE LENGHT: " + file.length);
+
+        return super.onSubmit(command);
+
+    }
+}
